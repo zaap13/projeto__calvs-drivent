baed2134b2c33858cb72d560271016e44d899a45
Felipe Bueno <felipebueno2201@gmail.com>
2023-02-11 04:50:40 -0300

    body validation

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { authenticateToken, validateBody } from '@/middlewares';
-import { createBooking, getBooking } from '@/controllers';
+import { createBooking, getBooking, updateBooking } from '@/controllers';
 import { createBookingSchema } from '@/schemas';
 
 const bookingRouter = Router();
@@ -8,7 +8,8 @@ const bookingRouter = Router();
 bookingRouter
   .all('/*', authenticateToken)
   .get('/', getBooking)
-  .post('/', validateBody(createBookingSchema), createBooking)
-  .put('/:bookingId');
+  .use(validateBody(createBookingSchema))
+  .post('/', createBooking)
+  .put('/:bookingId', updateBooking);
 
 export { bookingRouter };
